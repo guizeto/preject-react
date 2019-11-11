4993759a26af50fa2f5e3b4727fcc359dbcb3929
Guizeto <g.luizeto@hotmail.com>
2019-11-11 19:37:47 -0300

    Exibindo Repositório

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import Container from '../../components/Container';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
@@ -84,7 +85,10 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <Link to={`/repository/${encodeURIComponent(repository.name)}`}> Detalhes </Link>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                {' '}
+                Detalhes{' '}
+              </Link>
             </li>
           ))}
         </List>
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-import { Loading } from './styles';
+import { Loading, Owner } from './styles';
+
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -41,12 +44,21 @@ export default class Repository extends Component {
   }
 
   render() {
-    const { repository, issues, loading} = this.state;
+    const { repository, issues, loading } = this.state;
 
     if (loading) {
       return <Loading>Carregando...</Loading>;
     }
 
-    return <h1>Repository</h1>;
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
